3a051e4cd5920f08a855778dc619c4ec1c8e3575
Andrii Nakryiko <andrii@kernel.org>
2022-06-29 15:03:20 -0700

    [EXPERIMENTAL] BPF-based RSocket frame parsing and notification delay
    Very early prototype that understands RSocket frames and delays
    notification until entire packet is assembled inside sk_buff.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- examples/c/bootstrap.bpf.c
+++ examples/c/bootstrap.bpf.c
@@ -1,111 +1,226 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /* Copyright (c) 2020 Facebook */
-#include "vmlinux.h"
+#include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 #include "bootstrap.h"
+
+extern int bpf_dynptr_from_skb(struct sk_buff *skb, __u64 flags,
+                               struct bpf_dynptr *ptr__uninit) __ksym __weak;
+extern void *bpf_dynptr_slice(const struct bpf_dynptr *ptr, uint32_t offset,
+                                   void *buffer, uint32_t buffer__sz) __ksym __weak;
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 8192);
-	__type(key, pid_t);
-	__type(value, u64);
-} exec_start SEC(".maps");
-
-struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 256 * 1024);
+	__uint(max_entries, 4096);
 } rb SEC(".maps");
 
-const volatile unsigned long long min_duration_ns = 0;
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKMAP);
+	__type(key, int);
+	__type(value, int);
+	__uint(max_entries, 4096);
+} sockmap SEC(".maps");
 
-SEC("tp/sched/sched_process_exec")
-int handle_exec(struct trace_event_raw_sched_process_exec *ctx)
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKHASH);
+	__type(key, __u64);
+	__type(value, int);
+	__uint(max_entries, 4096);
+} sockhash SEC(".maps");
+
+struct pos {
+	__u64 next_pos;
+	__u64 cnt;
+	__u64 total;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, __u64);
+	__type(value, struct pos);
+	__uint(max_entries, 4096);
+} posmap SEC(".maps");
+
+int port;
+bool verbose;
+bool transparent;
+int coalesce_len;
+int conn_cnt;
+
+SEC("sk_skb/stream_parser")
+int skb_parser(struct __sk_buff *skb)
 {
-	struct task_struct *task;
-	unsigned fname_off;
-	struct event *e;
-	pid_t pid;
-	u64 ts;
+	__u64 cookie = bpf_get_socket_cookie(skb);
+	struct pos *p = bpf_map_lookup_elem(&posmap, &cookie);
+	struct sk_skb_cb *skb_cb = (void *)((struct sk_buff *)skb)->cb;
+	union {
+		struct {
+			char zero;
+			char len[3];
+		};
+		__u32 frlen;
+	} frame_buf = {};
+	int cnt, len, off, err;
+	__u8 *data, *data_end;
+	struct bpf_dynptr skbp __attribute__((aligned(8)));
 
-	/* remember time exec() was executed for this PID */
-	pid = bpf_get_current_pid_tgid() >> 32;
-	ts = bpf_ktime_get_ns();
-	bpf_map_update_elem(&exec_start, &pid, &ts, BPF_ANY);
-
-	/* don't emit exec events when minimum duration is specified */
-	if (min_duration_ns)
+	if (!p) {
+		bpf_printk("PARSER FAIL cookie=%d remote_port=%d",
+			cookie, bpf_ntohl(skb->remote_port));
 		return 0;
+	}
 
-	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
+	if (verbose && p->cnt < 10) {
+		bpf_printk("PARSER STRP STATE full_len %d offset %d accum_len %d",
+			   BPF_CORE_READ(skb_cb, strp.strp.full_len),
+			   BPF_CORE_READ(skb_cb, strp.strp.offset),
+			   BPF_CORE_READ(skb_cb, strp.accum_len));
+	}
+
+	/*
+	err = bpf_dynptr_from_skb((void*)skb, 0, &skbp);
+	if (err) {
+		bpf_printk("FAILED TO CREATE DYNPTR: %d", err);
+		return SK_DROP;
+	}
+
+	off = BPF_CORE_READ(skb_cb, strp.strp.offset);
+	data = bpf_dynptr_slice(&skbp, off, &frame_buf.len, 3);
+	if (!data) {
+		bpf_printk("SLICE FAIL %d+3", off);
 		return 0;
+	}
 
-	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+	len = ((__u32)data[0] << 16) + ((__u32)data[1] << 8) + data[2];
+	if (verbose && p->cnt < 10) {
+		bpf_printk("DYNPTR FRAME LEN %d(+3 = %d) (off %d)",
+			len, len+3, off);
+	}
+	len += 3;
+	*/
 
-	e->exit_event = false;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	off = BPF_CORE_READ(skb_cb, strp.strp.offset);
+	if (bpf_skb_load_bytes(skb, off, &frame_buf.len, 3)) {
+		if (p->cnt < 10) {
+			bpf_printk("FAILED TO LOAD BYTES AT %d+3 skb_len %d",
+				off, skb->len);
+		}
+		return 0;
+	}
 
-	fname_off = ctx->__data_loc_filename & 0xFFFF;
-	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx + fname_off);
+	len = bpf_ntohl(frame_buf.frlen);
+	if (verbose && p->cnt < 10) {
+		bpf_printk("FRAME LEN %d(+3 = %d) (off %d)",
+			len, len+3, off);
+	}
+	len += 3;
 
-	/* successfully submit it to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
-	return 0;
+	if (verbose && p->cnt < 10) {
+		bpf_printk("PARSER PASS total=%lu skb_len=%d off=%d len=%d",
+			   p->total, skb->len, off, len);
+	}
+	p->cnt += 1;
+	return len;
 }
 
-SEC("tp/sched/sched_process_exit")
-int handle_exit(struct trace_event_raw_sched_process_template *ctx)
+SEC("sk_skb/stream_verdict")
+int skb_verdict(struct __sk_buff *skb)
 {
-	struct task_struct *task;
-	struct event *e;
-	pid_t pid, tid;
-	u64 id, ts, *start_ts, duration_ns = 0;
+	__u64 cookie = bpf_get_socket_cookie(skb);
+	struct pos *p = bpf_map_lookup_elem(&posmap, &cookie);
+	struct sk_skb_cb *skb_cb = (void *)((struct sk_buff *)skb)->cb;
+	int full_len;
+	
+	if (!p) {
+		bpf_printk("VERDICT FAIL cookie=%d remote_port=%d",
+			cookie, bpf_ntohl(skb->remote_port));
+		return 0;
+	}
 
-	/* get PID and TID of exiting thread/process */
-	id = bpf_get_current_pid_tgid();
-	pid = id >> 32;
-	tid = (u32)id;
+	full_len = BPF_CORE_READ(skb_cb, strp.strp.full_len),
+	p->total += full_len;
 
-	/* ignore thread exits */
-	if (pid != tid)
+	if (verbose && p->cnt < 10) {
+
+		bpf_printk("VERDICT STRP STATE full_len %d offset %d accum_len %d",
+			   BPF_CORE_READ(skb_cb, strp.strp.full_len),
+			   BPF_CORE_READ(skb_cb, strp.strp.offset),
+			   BPF_CORE_READ(skb_cb, strp.accum_len));
+	}
+
+	if (verbose && p->cnt < 10) {
+		bpf_printk("VERDICT PASS sock_cookie=%d local_port=%d remote_port=%d skb_len=%d total=%lu",
+			   cookie, skb->local_port, bpf_ntohl(skb->remote_port), skb->len,
+			   p->total);
+	}
+	return SK_PASS;
+}
+
+
+SEC("sockops")
+int sock_ops(struct bpf_sock_ops *ctx)
+{
+	int cur_pid = 0; //bpf_get_current_pid_tgid() >> 32;
+	int op = (int)ctx->op;
+	int local_port = ctx->local_port;
+	int remote_port = bpf_ntohl(ctx->remote_port);
+
+	if (local_port != port && remote_port != port)
 		return 0;
 
-	/* if we recorded start of the process, calculate lifetime duration */
-	start_ts = bpf_map_lookup_elem(&exec_start, &pid);
-	if (start_ts)
-		duration_ns = bpf_ktime_get_ns() - *start_ts;
-	else if (min_duration_ns)
-		return 0;
-	bpf_map_delete_elem(&exec_start, &pid);
+	switch (op) {
+	default:
+	case BPF_SOCK_OPS_TIMEOUT_INIT:
+	case BPF_SOCK_OPS_RWND_INIT:
+	case BPF_SOCK_OPS_NEEDS_ECN:
+	case BPF_SOCK_OPS_BASE_RTT:
+	case BPF_SOCK_OPS_RTO_CB:
+	case BPF_SOCK_OPS_RETRANS_CB:
+		break;
 
-	/* if process didn't live long enough, return early */
-	if (min_duration_ns && duration_ns < min_duration_ns)
-		return 0;
+	case BPF_SOCK_OPS_TCP_LISTEN_CB:
+		bpf_printk("SOCKOP pid=%d TCP_LISTEN local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+		bpf_printk("SOCKOP pid=%d PASSIVE_ESTABLISHED local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_TCP_CONNECT_CB:
+		bpf_printk("SOCKOP pid=%d TCP_CONNECT local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		bpf_printk("SOCKOP pid=%d ACTIVE_ESTABLISHED local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	}
 
-	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
-		return 0;
+	if (local_port == port && op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
+		__u64 sock_cookie = bpf_get_socket_cookie(ctx);
+		struct pos p = { .next_pos = 0 };
+		int err;
 
-	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+		err = bpf_map_update_elem(&posmap, &sock_cookie, &p, BPF_NOEXIST);
+		if (err) {
+			bpf_printk("POS ADD failed remote_port=%d err=%d conn_cnt=%d", remote_port, err, conn_cnt);
+			return 0;
+		}
 
-	e->exit_event = true;
-	e->duration_ns = duration_ns;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	e->exit_code = (BPF_CORE_READ(task, exit_code) >> 8) & 0xff;
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+		err = bpf_sock_hash_update(ctx, &sockhash, &sock_cookie, BPF_NOEXIST);
+		if (err) {
+			bpf_printk("SOCK ADD failed remote_port=%d err=%d conn_cnt=%d", remote_port, err);
+			return 0;
+		}
 
-	/* send data to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
-	return 0;
+		int cnt = __sync_add_and_fetch(&conn_cnt, 1);
+		bpf_printk("SOCK ADDED sock_cookie=%lu remote_port=%d conn_cnt=%d", sock_cookie, remote_port, cnt);
+	}
+
+	return 1;
 }
--- examples/c/bootstrap.c
+++ examples/c/bootstrap.c
@@ -6,13 +6,31 @@
 #include <time.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 #include "bootstrap.h"
 #include "bootstrap.skel.h"
 
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define __unused __attribute__((unused))
+
+static struct bootstrap_bpf *skel;
+
 static struct env {
 	bool verbose;
-	long min_duration_ms;
-} env;
+	int port;
+	bool transparent;
+	int conn_cnt;
+} env = {
+	.conn_cnt = 4096,
+};
 
 const char *argp_program_version = "bootstrap 0.0";
 const char *argp_program_bug_address = "<bpf@vger.kernel.org>";
@@ -26,6 +44,10 @@ const char argp_program_doc[] = "BPF bootstrap demo application.\n"
 static const struct argp_option opts[] = {
 	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
 	{ "duration", 'd', "DURATION-MS", 0, "Minimum process duration (ms) to report" },
+	{ "port", 'p', "port", 0, "Server port filter" },
+	{ "coalesce", 'c', "BYTES", 0, "Coalesce len in bytes" },
+	{ "transparent", 't', NULL, 0, "Verbose debug output" },
+	{ "conn-cnt", 'n', "CNT", 0, "Number of connection" },
 	{},
 };
 
@@ -35,11 +57,22 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 	case 'v':
 		env.verbose = true;
 		break;
-	case 'd':
+	case 't':
+		env.transparent = true;
+		break;
+	case 'p':
 		errno = 0;
-		env.min_duration_ms = strtol(arg, NULL, 10);
-		if (errno || env.min_duration_ms <= 0) {
-			fprintf(stderr, "Invalid duration: %s\n", arg);
+		env.port = strtol(arg, NULL, 10);
+		if (errno || env.port <= 0) {
+			fprintf(stderr, "Invalid port: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case 'n':
+		errno = 0;
+		env.conn_cnt = strtol(arg, NULL, 10);
+		if (errno || env.conn_cnt <= 0) {
+			fprintf(stderr, "Invalid conn_cnt: %s\n", arg);
 			argp_usage(state);
 		}
 		break;
@@ -60,7 +93,7 @@ static const struct argp argp = {
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	if (level == LIBBPF_DEBUG && !env.verbose)
+	if (level == LIBBPF_DEBUG /*&& !env.verbose*/)
 		return 0;
 	return vfprintf(stderr, format, args);
 }
@@ -69,6 +102,7 @@ static volatile bool exiting = false;
 
 static void sig_handler(int sig)
 {
+	exit(1);
 	exiting = true;
 }
 
@@ -97,10 +131,45 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	return 0;
 }
 
+static void setup(struct bootstrap_bpf *skel)
+{
+	int cg_fd;
+	int err;
+
+	cg_fd = open("/sys/fs/cgroup", O_RDONLY);
+	if (cg_fd < 0) {
+		fprintf(stderr, "Failed to get root CG FD: %d\n", -errno);
+		exit(1);
+	}
+
+	err = bpf_prog_attach(bpf_program__fd(skel->progs.skb_parser),
+			      bpf_map__fd(skel->maps.sockhash), 
+			      BPF_SK_SKB_STREAM_PARSER, 0);
+	if (err) {
+		fprintf(stderr, "Failed to attach STREAM_PARSER program: %d\n", err);
+		exit(1);
+	}
+
+	err = bpf_prog_attach(bpf_program__fd(skel->progs.skb_verdict),
+			      bpf_map__fd(skel->maps.sockhash), 
+			      BPF_SK_SKB_STREAM_VERDICT, 0);
+	if (err) {
+		fprintf(stderr, "Failed to attach STREAM_VERDICT program: %d\n", err);
+		exit(1);
+	}
+
+	skel->links.sock_ops = bpf_program__attach_cgroup(
+			skel->progs.sock_ops, cg_fd);
+	if (!skel->links.sock_ops) {
+		fprintf(stderr, "Failed to attach sockops: %d\n", -errno);
+		exit(1);
+	}
+	scanf("%d", &err);
+}
+
 int main(int argc, char **argv)
 {
 	struct ring_buffer *rb = NULL;
-	struct bootstrap_bpf *skel;
 	int err;
 
 	/* Parse command line arguments */
@@ -115,6 +184,9 @@ int main(int argc, char **argv)
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
+	printf("TRACING PORT %d\n", env.port);
+	printf("MAX CONN CNT %d\n", env.conn_cnt);
+
 	/* Load and verify BPF application */
 	skel = bootstrap_bpf__open();
 	if (!skel) {
@@ -122,8 +194,12 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	/* Parameterize BPF code with minimum duration parameter */
-	skel->rodata->min_duration_ns = env.min_duration_ms * 1000000ULL;
+	skel->bss->port = env.port;
+	skel->bss->verbose = env.verbose;
+	skel->bss->transparent = env.transparent;
+
+	bpf_map__set_max_entries(skel->maps.sockhash, env.conn_cnt);
+	bpf_map__set_max_entries(skel->maps.posmap, env.conn_cnt);
 
 	/* Load & verify BPF programs */
 	err = bootstrap_bpf__load(skel);
@@ -132,12 +208,9 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	/* Attach tracepoints */
-	err = bootstrap_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
-	}
+	setup(skel);
+
+	return 0;
 
 	/* Set up ring buffer polling */
 	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
