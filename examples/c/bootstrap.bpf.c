d2bade29dbb203e128edd1f98abe9d4ed747f341
Andrii Nakryiko <andrii@kernel.org>
2022-06-29 15:03:20 -0700

    [EXPERIMENTAL] BPF-based RSocket frame parsing and notification delay
    Very early prototype that understands RSocket frames and delays
    notification until entire packet is assembled inside sk_buff.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- examples/c/bootstrap.bpf.c
+++ examples/c/bootstrap.bpf.c
@@ -1,111 +1,399 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /* Copyright (c) 2020 Facebook */
-#include "vmlinux.h"
+#include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 #include "bootstrap.h"
+
+#define VLOG_MAX_CNT 1000000
+
+extern int bpf_dynptr_from_skb(struct sk_buff *skb, __u64 flags,
+                               struct bpf_dynptr *ptr__uninit) __ksym __weak;
+extern void *bpf_dynptr_slice(const struct bpf_dynptr *ptr, uint32_t offset,
+                                   void *buffer, uint32_t buffer__sz) __ksym __weak;
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 8192);
-	__type(key, pid_t);
-	__type(value, u64);
-} exec_start SEC(".maps");
-
-struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 256 * 1024);
+	__uint(max_entries, 4096);
 } rb SEC(".maps");
 
-const volatile unsigned long long min_duration_ns = 0;
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKMAP);
+	__type(key, int);
+	__type(value, int);
+	__uint(max_entries, 4096);
+} sockmap SEC(".maps");
 
-SEC("tp/sched/sched_process_exec")
-int handle_exec(struct trace_event_raw_sched_process_exec *ctx)
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKHASH);
+	__type(key, __u64);
+	__type(value, int);
+	__uint(max_entries, 4096);
+} sockhash SEC(".maps");
+
+struct pos {
+	__u64 next_pos;
+	__u64 cnt;
+	__u64 total;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, __u64);
+	__type(value, struct pos);
+	__uint(max_entries, 4096);
+} posmap SEC(".maps");
+
+int port;
+bool verbose;
+bool transparent;
+int coalesce_len;
+int conn_cnt;
+
+int pid;
+static __u64 seq;
+
+#define EV_MSK (((1 << 14) - 1))
+
+enum evtype
 {
-	struct task_struct *task;
-	unsigned fname_off;
-	struct event *e;
-	pid_t pid;
-	u64 ts;
+	EV_INVAL,
+	EV_EPOLL_WAIT_RET,
+	EV_RECVFROM_RET,
+	EV_PARSE,
+	EV_VERDICT,
+};
 
-	/* remember time exec() was executed for this PID */
-	pid = bpf_get_current_pid_tgid() >> 32;
-	ts = bpf_ktime_get_ns();
-	bpf_map_update_elem(&exec_start, &pid, &ts, BPF_ANY);
+struct ev {
+	__u64 seq;
+	enum evtype t;
+	//__u64 ts;
+	
+	int retval;
 
-	/* don't emit exec events when minimum duration is specified */
-	if (min_duration_ns)
+	int fr_num;
+	int skb_len;
+	int fr_len;
+	int strp_full_len;
+	int strp_off;
+	int strp_accum_len;
+	int sk_rcvbuf;
+	int sk_rmem_alloc;
+	int sk_backlog_len;
+};
+
+static struct ev evs[EV_MSK + 1];
+
+static struct ev *getev(void)
+{
+	__u64 sq = __sync_fetch_and_add(&seq, 1);
+	struct ev *ev = &evs[sq & EV_MSK];
+
+	ev->t = EV_INVAL;
+	ev->seq = sq;
+	//ev->ts = bpf_ktime_get_ns();
+
+	return ev;
+}
+
+static void dump_state(__u64 seq)
+{
+	__u64 i;
+
+	for (i = 0; i < 10; i++) {
+		__u64 s = (seq - i) & EV_MSK;
+		struct ev *ev = &evs[s];
+
+		switch (ev->t) {
+		default:
+		case EV_INVAL:
+			bpf_printk("[%lu] ???", ev->seq);
+			break;
+		case EV_EPOLL_WAIT_RET:
+			bpf_printk("[%lu] EPWAIT_RET retval=%d", ev->seq, ev->retval);
+			break;
+		case EV_RECVFROM_RET:
+			bpf_printk("[%lu] RECVFROM_RET retval=%d", ev->seq, ev->retval);
+			break;
+		case EV_PARSE:
+			bpf_printk("[%lu] PARSE fr#%d fr_len %d skb_len %d sk_rcvbuf %d sk_rmem_alloc %d sk_backlog_len %d strp (full %d, off %d, accum %d)",
+				ev->seq, ev->fr_num, ev->fr_len, ev->skb_len,
+				ev->sk_rcvbuf, ev->sk_rmem_alloc, ev->sk_backlog_len,
+				ev->strp_full_len, ev->strp_off, ev->strp_accum_len);
+			break;
+		case EV_VERDICT:
+			bpf_printk("[%lu] VERDICT fr#%d fr_len %d skb_len %d sk_rcvbuf %d sk_rmem_alloc %d sk_backlog_len %d strp (full %d, off %d, accum %d)",
+				ev->seq, ev->fr_num, ev->fr_len, ev->skb_len,
+				ev->sk_rcvbuf, ev->sk_rmem_alloc, ev->sk_backlog_len,
+				ev->strp_full_len, ev->strp_off, ev->strp_accum_len);
+			break;
+		}
+	}
+}
+
+SEC("sk_skb/stream_parser")
+int skb_parser(struct __sk_buff *skb)
+{
+	__u64 cookie = bpf_get_socket_cookie(skb);
+	struct pos *p = bpf_map_lookup_elem(&posmap, &cookie);
+	struct sk_skb_cb *skb_cb = (void *)((struct sk_buff *)skb)->cb;
+	union {
+		struct {
+			char zero;
+			char len[3];
+		};
+		__u32 frlen;
+	} frame_buf = {};
+	int cnt, len, off, err;
+	__u8 *data, *data_end;
+	struct bpf_dynptr skbp __attribute__((aligned(8)));
+
+	if (!p) {
+		bpf_printk("PARSER FAIL cookie=%d remote_port=%d",
+			cookie, bpf_ntohl(skb->remote_port));
+		return 0;
+	}
+
+	if (transparent)
+		return skb->len;
+
+	if (verbose && p->cnt < VLOG_MAX_CNT) {
+		bpf_printk("PARSER STRP STATE full_len %d offset %d accum_len %d",
+			   BPF_CORE_READ(skb_cb, strp.strp.full_len),
+			   BPF_CORE_READ(skb_cb, strp.strp.offset),
+			   BPF_CORE_READ(skb_cb, strp.accum_len));
+	}
+
+	/*
+	err = bpf_dynptr_from_skb((void*)skb, 0, &skbp);
+	if (err) {
+		bpf_printk("FAILED TO CREATE DYNPTR: %d", err);
+		return SK_DROP;
+	}
+
+	off = BPF_CORE_READ(skb_cb, strp.strp.offset);
+	data = bpf_dynptr_slice(&skbp, off, &frame_buf.len, 3);
+	if (!data) {
+		bpf_printk("SLICE FAIL %d+3", off);
+		return 0;
+	}
+
+	len = ((__u32)data[0] << 16) + ((__u32)data[1] << 8) + data[2];
+	if (verbose && p->cnt < 10) {
+		bpf_printk("DYNPTR FRAME LEN %d(+3 = %d) (off %d)",
+			len, len+3, off);
+	}
+	len += 3;
+	*/
+
+	off = BPF_CORE_READ(skb_cb, strp.strp.offset);
+	if (bpf_skb_load_bytes(skb, off, &frame_buf.len, 3)) {
+		if (p->cnt < VLOG_MAX_CNT) {
+			bpf_printk("FAILED TO LOAD BYTES AT %d+3 skb_len %d",
+				off, skb->len);
+		}
+		return 0;
+	}
+
+	len = bpf_ntohl(frame_buf.frlen) + 3;
+
+	if (verbose && p->cnt < VLOG_MAX_CNT) {
+		bpf_printk("FRAME skb_len=%d off=%d len=%d more=%d",
+			skb->len, off, len, (off + len < skb->len) ? 1 : 0);
+	}
+	p->cnt += 1;
+
+	struct ev *ev = getev();
+
+	ev->t = EV_PARSE;
+	ev->fr_num = p->cnt - 1;
+	ev->skb_len = skb->len;
+	ev->fr_len = len;
+	ev->strp_full_len = BPF_CORE_READ(skb_cb, strp.strp.full_len);
+	ev->strp_off = off;
+	ev->strp_accum_len = BPF_CORE_READ(skb_cb, strp.accum_len);
+	ev->sk_rcvbuf = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_rcvbuf);
+	ev->sk_rmem_alloc = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_backlog.rmem_alloc.counter);
+	ev->sk_backlog_len = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_backlog.len);
+
+	return len;
+}
+
+SEC("sk_skb/stream_verdict")
+int skb_verdict(struct __sk_buff *skb)
+{
+	__u64 cookie = bpf_get_socket_cookie(skb);
+	struct pos *p = bpf_map_lookup_elem(&posmap, &cookie);
+	struct sk_skb_cb *skb_cb = (void *)((struct sk_buff *)skb)->cb;
+	int full_len;
+	
+	if (!p) {
+		bpf_printk("VERDICT FAIL cookie=%d remote_port=%d",
+			cookie, bpf_ntohl(skb->remote_port));
+		return 0;
+	}
+
+	full_len = BPF_CORE_READ(skb_cb, strp.strp.full_len),
+	p->total += full_len;
+
+	if (verbose && p->cnt < VLOG_MAX_CNT) {
+
+		bpf_printk("VERDICT STRP STATE full_len %d offset %d accum_len %d",
+			   BPF_CORE_READ(skb_cb, strp.strp.full_len),
+			   BPF_CORE_READ(skb_cb, strp.strp.offset),
+			   BPF_CORE_READ(skb_cb, strp.accum_len));
+	}
+
+	if (verbose && p->cnt < VLOG_MAX_CNT) {
+		bpf_printk("VERDICT PASS sock_cookie=%d local_port=%d remote_port=%d skb_len=%d total=%lu",
+			   cookie, skb->local_port, bpf_ntohl(skb->remote_port), skb->len,
+			   p->total);
+	}
+
+	struct ev *ev = getev();
+
+	ev->t = EV_VERDICT;
+	ev->fr_num = p->cnt - 1;
+	ev->skb_len = skb->len;
+	ev->fr_len = 0;
+	ev->strp_full_len = BPF_CORE_READ(skb_cb, strp.strp.full_len);
+	ev->strp_off = BPF_CORE_READ(skb_cb, strp.strp.offset);
+	ev->strp_accum_len = BPF_CORE_READ(skb_cb, strp.accum_len);
+	ev->sk_rcvbuf = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_rcvbuf);
+	ev->sk_rmem_alloc = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_backlog.rmem_alloc.counter);
+	ev->sk_backlog_len = BPF_CORE_READ((struct sk_buff *)skb, sk, sk_backlog.len);
+
+	return SK_PASS;
+}
+
+
+SEC("sockops")
+int sock_ops(struct bpf_sock_ops *ctx)
+{
+	int cur_pid = 0; //bpf_get_current_pid_tgid() >> 32;
+	int op = (int)ctx->op;
+	int local_port = ctx->local_port;
+	int remote_port = bpf_ntohl(ctx->remote_port);
+
+	if (local_port != port && remote_port != port)
 		return 0;
 
-	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
+	switch (op) {
+	default:
+	case BPF_SOCK_OPS_TIMEOUT_INIT:
+	case BPF_SOCK_OPS_RWND_INIT:
+	case BPF_SOCK_OPS_NEEDS_ECN:
+	case BPF_SOCK_OPS_BASE_RTT:
+	case BPF_SOCK_OPS_RTO_CB:
+	case BPF_SOCK_OPS_RETRANS_CB:
+		break;
+
+	case BPF_SOCK_OPS_TCP_LISTEN_CB:
+		bpf_printk("SOCKOP pid=%d TCP_LISTEN local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+		bpf_printk("SOCKOP pid=%d PASSIVE_ESTABLISHED local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_TCP_CONNECT_CB:
+		bpf_printk("SOCKOP pid=%d TCP_CONNECT local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		bpf_printk("SOCKOP pid=%d ACTIVE_ESTABLISHED local %d remote %d",
+			cur_pid, local_port, remote_port);
+		break;
+	}
+
+	if (local_port == port && op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
+		__u64 sock_cookie = bpf_get_socket_cookie(ctx);
+		struct pos p = { .next_pos = 0 };
+		int err;
+
+		err = bpf_map_update_elem(&posmap, &sock_cookie, &p, BPF_NOEXIST);
+		if (err) {
+			bpf_printk("POS ADD failed remote_port=%d err=%d conn_cnt=%d", remote_port, err, conn_cnt);
+			return 0;
+		}
+
+		err = bpf_sock_hash_update(ctx, &sockhash, &sock_cookie, BPF_NOEXIST);
+		if (err) {
+			bpf_printk("SOCK ADD failed remote_port=%d err=%d conn_cnt=%d", remote_port, err);
+			return 0;
+		}
+
+		int cnt = __sync_add_and_fetch(&conn_cnt, 1);
+		int sk_rcvbuf = BPF_CORE_READ((struct sk_buff *)ctx, sk, sk_rcvbuf);
+		bpf_printk("SOCK ADDED sock_cookie=%lu remote_port=%d sk_rcvbuf=%d conn_cnt=%d",
+			sock_cookie, remote_port, sk_rcvbuf, cnt);
+	}
+
+	return 1;
+}
+
+SEC("fexit/__x64_sys_epoll_wait")
+int BPF_PROG(epoll_wait_exit, struct pt_regs *regs, int retval)
+{
+	int cur_pid = bpf_get_current_pid_tgid() >> 32;
+	struct ev *ev;
+
+	if (pid == 0 || pid != cur_pid)
 		return 0;
 
-	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+	if (retval <= 0)
+		return 0;
 
-	e->exit_event = false;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	ev = getev();
 
-	fname_off = ctx->__data_loc_filename & 0xFFFF;
-	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx + fname_off);
+	ev->t = EV_EPOLL_WAIT_RET;
+	ev->retval = retval;
 
-	/* successfully submit it to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
 	return 0;
 }
 
-SEC("tp/sched/sched_process_exit")
-int handle_exit(struct trace_event_raw_sched_process_template *ctx)
+SEC("fexit/__x64_sys_recvfrom")
+int BPF_PROG(recvfrom_exit, struct pt_regs *regs, int retval)
 {
-	struct task_struct *task;
-	struct event *e;
-	pid_t pid, tid;
-	u64 id, ts, *start_ts, duration_ns = 0;
+	int cur_pid = bpf_get_current_pid_tgid() >> 32;
+	struct ev *ev;
 
-	/* get PID and TID of exiting thread/process */
-	id = bpf_get_current_pid_tgid();
-	pid = id >> 32;
-	tid = (u32)id;
-
-	/* ignore thread exits */
-	if (pid != tid)
+	if (pid == 0 || pid != cur_pid)
 		return 0;
 
-	/* if we recorded start of the process, calculate lifetime duration */
-	start_ts = bpf_map_lookup_elem(&exec_start, &pid);
-	if (start_ts)
-		duration_ns = bpf_ktime_get_ns() - *start_ts;
-	else if (min_duration_ns)
-		return 0;
-	bpf_map_delete_elem(&exec_start, &pid);
-
-	/* if process didn't live long enough, return early */
-	if (min_duration_ns && duration_ns < min_duration_ns)
+	if (retval >= 0)
 		return 0;
 
-	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
-		return 0;
+	ev = getev();
 
-	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+	ev->t = EV_RECVFROM_RET;
+	ev->retval = retval;
 
-	e->exit_event = true;
-	e->duration_ns = duration_ns;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	e->exit_code = (BPF_CORE_READ(task, exit_code) >> 8) & 0xff;
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	dump_state(ev->seq);
 
-	/* send data to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
 	return 0;
 }
+
+/*
+#define SOL_SOCKET 1
+#define SO_RCVLOWAT 18
+
+SEC("cgroup/skb")
+int handle_skb(struct __sk_buff *skb)
+{
+	int err, val;
+
+	if (skb->local_port != port)
+		return SK_PASS;
+
+	val = 16 * 1024;
+	err = bpf_setsockopt(skb, SOL_SOCKET, SO_RCVLOWAT, &val, sizeof(val));
+	if (err)
+		bpf_printk("SETSOCKOPT FAILED: %d", err);
+	return SK_PASS;
+}
+*/
--- examples/c/bootstrap.c
+++ examples/c/bootstrap.c
@@ -6,13 +6,35 @@
 #include <time.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 #include "bootstrap.h"
 #include "bootstrap.skel.h"
 
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+
+#define __unused __attribute__((unused))
+
+static struct bootstrap_bpf *skel;
+
 static struct env {
 	bool verbose;
-	long min_duration_ms;
-} env;
+	int pid;
+	int port;
+	bool transparent;
+	bool lowat_mode;
+	int conn_cnt;
+	bool manual;
+} env = {
+	.conn_cnt = 4096,
+};
 
 const char *argp_program_version = "bootstrap 0.0";
 const char *argp_program_bug_address = "<bpf@vger.kernel.org>";
@@ -25,7 +47,14 @@ const char argp_program_doc[] = "BPF bootstrap demo application.\n"
 
 static const struct argp_option opts[] = {
 	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ "manual", 'm', NULL, 0, "Manual client/server mode" },
+	{ "lowat", 'l', NULL, 0, "RCVLOWAT mode" },
 	{ "duration", 'd', "DURATION-MS", 0, "Minimum process duration (ms) to report" },
+	{ "port", 'p', "PORT", 0, "Server port filter" },
+	{ "pid", 'P', "PID", 0, "Server PID filter" },
+	{ "coalesce", 'c', "BYTES", 0, "Coalesce len in bytes" },
+	{ "transparent", 't', NULL, 0, "Verbose debug output" },
+	{ "conn-cnt", 'n', "CNT", 0, "Number of connection" },
 	{},
 };
 
@@ -35,11 +64,36 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 	case 'v':
 		env.verbose = true;
 		break;
-	case 'd':
+	case 'm':
+		env.manual = true;
+		break;
+	case 't':
+		env.transparent = true;
+		break;
+	case 'l':
+		env.lowat_mode = true;
+		break;
+	case 'P':
 		errno = 0;
-		env.min_duration_ms = strtol(arg, NULL, 10);
-		if (errno || env.min_duration_ms <= 0) {
-			fprintf(stderr, "Invalid duration: %s\n", arg);
+		env.pid = strtol(arg, NULL, 10);
+		if (errno || env.pid <= 0) {
+			fprintf(stderr, "Invalid PID: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case 'p':
+		errno = 0;
+		env.port = strtol(arg, NULL, 10);
+		if (errno || env.port <= 0) {
+			fprintf(stderr, "Invalid port: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case 'n':
+		errno = 0;
+		env.conn_cnt = strtol(arg, NULL, 10);
+		if (errno || env.conn_cnt <= 0) {
+			fprintf(stderr, "Invalid conn_cnt: %s\n", arg);
 			argp_usage(state);
 		}
 		break;
@@ -60,7 +114,7 @@ static const struct argp argp = {
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	if (level == LIBBPF_DEBUG && !env.verbose)
+	if (level == LIBBPF_DEBUG /*&& !env.verbose*/)
 		return 0;
 	return vfprintf(stderr, format, args);
 }
@@ -69,6 +123,7 @@ static volatile bool exiting = false;
 
 static void sig_handler(int sig)
 {
+	exit(1);
 	exiting = true;
 }
 
@@ -97,10 +152,323 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	return 0;
 }
 
+struct msg {
+	unsigned char len1, len2, len3;
+	char payload[];
+};
+
+static volatile bool server_started = false;
+
+static void *handle_server(void *arg)
+{
+	int port = env.port;
+	socklen_t client_addr_len;
+	struct sockaddr_in server_addr, client_addr;
+	int server_fd, client_fd, err;
+	char buf[512 * 1024];
+	struct epoll_event ev;
+	int epoll_fd;
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		fprintf(stderr, "Failed to create server socket: %d\n", -errno);
+		exit(1);
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(port);
+
+	err = bind(server_fd, (void *)&server_addr, sizeof(server_addr));
+	if (err) {
+		fprintf(stderr, "Failed to bind server socket fd %d: %d\n", server_fd, -errno);
+		exit(1);
+	}
+
+	/*
+	if (fcntl(server_fd, F_SETFL, fcntl(server_fd, F_GETFL, 0) | O_NONBLOCK) < 0) {
+		fprintf(stderr, "Failed to make server socket non-blocking: %d\n", -errno);
+		exit(1);
+	}
+	*/
+
+	err = listen(server_fd, 1);
+	if (err) {
+		fprintf(stderr, "Failed to listen socket fd %d: %d\n", server_fd, -errno);
+		exit(1);
+	}
+
+	printf("SERVER started at port %d\n", port);
+	server_started = true;
+
+again:
+	client_fd = accept(server_fd, (void *)&client_addr, &client_addr_len);
+	if (client_fd < 0 && errno == EAGAIN)
+		goto again;
+	if (client_fd < 0) {
+		fprintf(stderr, "Failed to accept client socket on server fd %d: %d\n", server_fd, -errno);
+		exit(1);
+	} 
+	if (fcntl(client_fd, F_SETFL, fcntl(client_fd, F_GETFL, 0) | O_NONBLOCK) < 0) {
+		fprintf(stderr, "Failed to make client socket non-blocking: %d\n", -errno);
+		exit(1);
+	}
+
+	epoll_fd = epoll_create1(EPOLL_CLOEXEC);
+	if (epoll_fd < 0) {
+		fprintf(stderr, "Failed to create epoll_fd: %d\n", -errno);
+		exit(1);
+	}
+
+	memset(&ev, 0, sizeof(ev));
+	ev.events = EPOLLIN;
+	ev.data.fd = 0;
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) < 0) {
+		fprintf(stderr, "Failed to EPOLL_CTL_ADD client_fd: %d\n", -errno);
+		exit(1);
+	}
+
+	int buf_len = 0;
+	while (true) {
+		const struct msg *msg = (void *)buf;
+		int msg_len, need_len;
+
+		err = epoll_wait(epoll_fd, &ev, 1, 100000);
+		if (err < 0 && errno == EAGAIN) {
+			fprintf(stdout, "EPOLL EAGAIN!\n");
+			continue;
+		}
+		if (err < 0) {
+			fprintf(stderr, "EPOLL ERROR: %d\n", -errno);
+			exit(1);
+		}
+		fprintf(stdout, "EPOLL RETURNED %d\n", err);
+
+		err = recvfrom(client_fd, buf + buf_len, sizeof(buf) - buf_len, MSG_DONTWAIT, NULL, NULL);
+		if (err < 0 && errno == EAGAIN) {
+			fprintf(stdout, "READ EAGAIN!\n");
+			continue;
+		}
+		if (err < 0) {
+			fprintf(stderr, "READ ERROR: %d, %d\n", err, -errno);
+			exit(1);
+		}
+		if (err == 0) {
+			fprintf(stdout, "READ ZERO!!!\n");
+			continue;
+		}
+		fprintf(stdout, "READ GOT %d\n", err);
+
+		buf_len += err;
+		while (buf_len > 0) {
+			if (buf_len < sizeof(*msg))
+				need_len = sizeof(*msg);
+			else
+				need_len = ((unsigned)msg->len1 << 16) +
+					((unsigned)msg->len2 << 8) +
+					(unsigned)msg->len3 +
+					sizeof(*msg);
+
+			if (buf_len < need_len) {
+				fprintf(stdout, "Got new %d bytes in buffer, need at least %d, waiting for more data...\n",
+					buf_len, need_len);
+				break;
+			}
+			msg_len = need_len;
+
+			fprintf(stdout, "RECV MSG len %d payload %s\n", need_len - 3, msg->payload);
+
+			/* shift the remainder so it's always at the beginning of the buf */
+			memmove(buf, buf + msg_len, buf_len - msg_len);
+			msg = (void *)&buf;
+			buf_len -= msg_len;
+		}
+	}
+
+	printf("SERVER shutting down, last read got error %d %d...\n", err, -errno);
+
+	return NULL;
+}
+
+static void *handle_client(void *arg)
+{
+	int port = env.port;
+	int sock_fd, err;
+	struct sockaddr_in server_addr;
+	char buf[512 * 1024];
+	struct msg *msg = (void *)&buf;
+
+	fprintf(stdout, "CLIENT starting...\n");
+
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd < 0) {
+		fprintf(stderr, "Failed to create client socket: %d\n", -errno);
+		exit(1);
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(port);
+
+	err = connect(sock_fd, (void *)&server_addr, sizeof(server_addr));
+	if (err) {
+		fprintf(stderr, "Failed to connect client socket fd %d: %d\n", sock_fd, -errno);
+		exit(1);
+	}
+
+	fprintf(stdout, "CLIENT started for port %d\n", port);
+
+	while (true) {
+		int msg_lens[16];
+		char msg_chars[16];
+		char msg_char;
+		int msg_len, rem, sent, chunk;
+		int msg_cnt = 0, i, buf_len;
+
+		fprintf(stdout, "MSGs (len1 len2 ... char1 char2 ...): \n");
+		while (scanf("%d", &msg_lens[msg_cnt]) == 1) {
+			msg_cnt++;
+			if (msg_cnt > 16) {
+				fprintf(stderr, "TOO MANY SIMULTANEOUS MSGS...\n");
+				exit(1);
+			}
+		}
+		for (i = 0; i < msg_cnt; i++) {
+			scanf(" %c", &msg_chars[i]);
+		}
+		
+		buf_len = 0;
+		for (i = 0; i < msg_cnt; i++) {
+			msg_len = msg_lens[i];
+			msg_char = msg_chars[i];
+
+			fprintf(stdout, "BUF_LEN %d MSG_LEN %d\n", buf_len, msg_len);
+			msg = (void *)&buf[buf_len];
+
+			msg->len1 = (msg_len >> 16) & 0xFF;
+			msg->len2 = (msg_len >> 8) & 0xFF;
+			msg->len3 = msg_len & 0xFF;
+
+			fprintf(stdout, "L1 %u L2 %u L3 %u\n",
+				(unsigned)msg->len1,
+				(unsigned)msg->len2,
+				(unsigned)msg->len3);
+
+			memset(msg->payload, msg_char, msg_len);
+			msg->payload[msg_len - 1] = '\0';
+
+			fprintf(stdout, "PREPARING MSG total %d+3(=%d) bytes (%d * '%c')...\n",
+				msg_len, msg_len + 3, msg_len, msg_char);
+
+			msg_len += 3;
+			buf_len += msg_len;
+		}
+
+		fprintf(stdout, "SENDING PAYLOAD total %d bytes...\n", buf_len);
+
+		rem = buf_len;
+		sent = 0;
+		while (rem && scanf("%d", &chunk) == 1) {
+			if (chunk > rem)
+				chunk = rem;
+			err = write(sock_fd, buf + sent, chunk);
+			if (err < 0 || err != chunk) {
+				fprintf(stderr, "Failed to send message (msg_len %d, sent %d, rem %d, chunk %d, ret %d): %d\n",
+					msg_len, sent, rem, chunk, err, -errno);
+				exit(1);
+			}
+			rem -= chunk;
+			sent += chunk;
+		}
+
+		fprintf(stdout, "DONE SENDING.\n");
+	}
+
+	return NULL;
+}
+
+static void setup_client_server(void)
+{
+	pthread_t client_thread, server_thread;
+	int err;
+
+	err = pthread_create(&server_thread, NULL, handle_server, NULL);
+	if (err) {
+		fprintf(stderr, "Failed to create server thread: %d\n", err);
+		exit(1);
+	}
+
+	fprintf(stdout, "Waiting for server to start up...\n");
+	while (!server_started) {
+		usleep(100);
+	}
+
+	err = pthread_create(&client_thread, NULL, handle_client, NULL);
+	if (err) {
+		fprintf(stderr, "Failed to create client thread: %d\n", err);
+		exit(1);
+	}
+
+	fprintf(stdout, "Joining client and server threads...\n");
+	pthread_join(client_thread, NULL);
+	fprintf(stdout, "Client thread finished!\n");
+	pthread_join(server_thread, NULL);
+	fprintf(stdout, "Server thread finished!\n");
+
+	fprintf(stdout, "Exiting...\n");
+}
+
+static void setup(struct bootstrap_bpf *skel)
+{
+	int cg_fd;
+	int err;
+
+	cg_fd = open("/sys/fs/cgroup", O_RDONLY);
+	if (cg_fd < 0) {
+		fprintf(stderr, "Failed to get root CG FD: %d\n", -errno);
+		exit(1);
+	}
+
+	/*
+	if (env.lowat_mode) {
+		skel->links.handle_skb = bpf_program__attach_cgroup(skel->progs.handle_skb, cg_fd);
+		if (!skel->links.handle_skb) {
+			fprintf(stderr, "Failed to attach handle_skb: %d\n", -errno);
+			exit(1);
+		}
+		return;
+	}
+	*/
+
+	err = bpf_prog_attach(bpf_program__fd(skel->progs.skb_parser),
+			      bpf_map__fd(skel->maps.sockhash), 
+			      BPF_SK_SKB_STREAM_PARSER, 0);
+	if (err) {
+		fprintf(stderr, "Failed to attach STREAM_PARSER program: %d\n", err);
+		exit(1);
+	}
+
+	err = bpf_prog_attach(bpf_program__fd(skel->progs.skb_verdict),
+			      bpf_map__fd(skel->maps.sockhash), 
+			      BPF_SK_SKB_STREAM_VERDICT, 0);
+	if (err) {
+		fprintf(stderr, "Failed to attach STREAM_VERDICT program: %d\n", err);
+		exit(1);
+	}
+
+	skel->links.sock_ops = bpf_program__attach_cgroup(
+			skel->progs.sock_ops, cg_fd);
+	if (!skel->links.sock_ops) {
+		fprintf(stderr, "Failed to attach sockops: %d\n", -errno);
+		exit(1);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	struct ring_buffer *rb = NULL;
-	struct bootstrap_bpf *skel;
 	int err;
 
 	/* Parse command line arguments */
@@ -115,6 +483,10 @@ int main(int argc, char **argv)
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
+	printf("TRACING PORT %d\n", env.port);
+	printf("TRACING PID %d\n", env.pid);
+	printf("MAX CONN CNT %d\n", env.conn_cnt);
+
 	/* Load and verify BPF application */
 	skel = bootstrap_bpf__open();
 	if (!skel) {
@@ -122,8 +494,13 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	/* Parameterize BPF code with minimum duration parameter */
-	skel->rodata->min_duration_ns = env.min_duration_ms * 1000000ULL;
+	skel->bss->port = env.port;
+	skel->bss->pid = env.pid;
+	skel->bss->verbose = env.verbose;
+	skel->bss->transparent = env.transparent;
+
+	bpf_map__set_max_entries(skel->maps.sockhash, env.conn_cnt);
+	bpf_map__set_max_entries(skel->maps.posmap, env.conn_cnt);
 
 	/* Load & verify BPF programs */
 	err = bootstrap_bpf__load(skel);
@@ -132,12 +509,18 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	/* Attach tracepoints */
 	err = bootstrap_bpf__attach(skel);
 	if (err) {
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
 	}
+
+	setup(skel);
+	if (env.manual)
+		setup_client_server();
+
+	scanf("%d", &err);
+	return 0;
 
 	/* Set up ring buffer polling */
 	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
