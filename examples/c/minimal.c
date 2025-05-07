cbeebed29aaceeab17504306b1c78ce8ef75ad14
Andrii Nakryiko <andrii@kernel.org>
2025-05-07 11:12:27 -0700

    repro: create big map in a loop
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- examples/c/minimal.c
+++ examples/c/minimal.c
@@ -2,57 +2,30 @@
 /* Copyright (c) 2020 Facebook */
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/resource.h>
-#include <bpf/libbpf.h>
-#include "minimal.skel.h"
-
-static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
-{
-	return vfprintf(stderr, format, args);
-}
+#include <bpf/bpf.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	struct minimal_bpf *skel;
-	int err;
-
-	/* Set up libbpf errors and debug info callback */
-	libbpf_set_print(libbpf_print_fn);
-
-	/* Open BPF application */
-	skel = minimal_bpf__open();
-	if (!skel) {
-		fprintf(stderr, "Failed to open BPF skeleton\n");
-		return 1;
-	}
-
-	/* ensure BPF program only handles write() syscalls from our process */
-	skel->bss->my_pid = getpid();
-
-	/* Load & verify BPF programs */
-	err = minimal_bpf__load(skel);
-	if (err) {
-		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-		goto cleanup;
-	}
-
-	/* Attach tracepoint handler */
-	err = minimal_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
-	}
-
-	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-	       "to see output of the BPF programs.\n");
+	int cnt = 0;
 
 	for (;;) {
-		/* trigger our BPF program */
-		fprintf(stderr, ".");
-		sleep(1);
+		int min_pages = 4096 / 8;
+		int key_sz = 8;
+		int val_sz = 1024 - 8;
+		int min_elems = min_pages * 4096 / (key_sz + val_sz);
+
+		int num_elems = min_elems + 100 * min_elems * ((rand() + 0.0) / RAND_MAX);
+		int fd = bpf_map_create(BPF_MAP_TYPE_HASH, "boom_map", key_sz, val_sz, num_elems, NULL);
+		if (fd >= 0)
+			close(fd);
+		printf("MIN_ELEMS %d NUM_ELEMS %d APPROX_SZ %d FD %d\n",
+		       min_elems, num_elems, num_elems * (key_sz + val_sz), fd);
+
+		if (cnt++ % 100 == 0)
+			fprintf(stderr, ".");
+		usleep(1);
 	}
 
-cleanup:
-	minimal_bpf__destroy(skel);
-	return -err;
+	return 0;
 }
